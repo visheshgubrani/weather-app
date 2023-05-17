88ec19c2463a116a5b16c754c4305e69d0b3f641
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-05-17 23:27:05 +0530

    Created Initial File that takes response from API

---
+++ weather_app.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+base_url = "http://api.weatherapi.com/v1"
+endpoint = "/current.json"
+api_key = "e69e523d9ed04abf8a2162555231705"
+
+request_url = f"{base_url}{endpoint}?key={api_key}&q=India"
+
+response = requests.get(request_url)
+
+weather_data = response.json()
+
+current_temp = weather_data["current"]["temp_c"]
+
+print(f"Temprature of India is {current_temp}C")
