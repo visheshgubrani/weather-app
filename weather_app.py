96e761903c31a8c5d23a2fc0e78974fd6c4c4e80
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-05-20 15:57:04 +0530

    Fixed forecast

--- weather_app.py
+++ weather_app.py
@@ -12,14 +12,12 @@ def get_current_weather(country):
     current_temp = weather_data["current"]["temp_c"]
     return current_temp
 
-def get_forecast(city):
+def get_forecast(city, days):
     endpoint = "/forecast.json"
-    request_url = f"{base_url}{endpoint}?key={api_key}&q={city}"
+    request_url = f"{base_url}{endpoint}?key={api_key}&q={city}&days={days}"
     response = requests.get(request_url)
     forecast_data = response.json()
-    forecast_day = forecast_data["forecast"]["forecastday"][0]["date"]
-    forecast_temp = forecast_data["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
-    return forecast_day, forecast_temp
+    return forecast_data
 
 def get_air_quality(city):
     endpoint = "/current.json"
@@ -32,13 +30,24 @@ def get_air_quality(city):
 def main():
     country = input("Select the country: ")
     city = input("Select the city: ")
+    num_days = int(input("Enter the No. of days: "))
+    
 
     current_temp = get_current_weather(country)
-    forecast_day, forecast_temp = get_forecast(city)
-    air_quality_index = get_air_quality(city)
-
     print(f"Temprature in {city}, {country} is {current_temp}C")
-    print(f"Forecast for {city} on {forecast_day} is {forecast_temp}C")
+    
+    def print_forecast(forecast_data):
+        forecast_days = forecast_data["forecast"]["forecastday"]
+
+        for day in forecast_days:
+            forecast_date = day["date"]
+            forecast_temp = day["day"]["avgtemp_c"]
+            print(f"forecast for {forecast_date}: {forecast_temp}C")
+    forecast_data = get_forecast(city, num_days)
+    print_forecast(forecast_data)
+    
+    
+    air_quality_index = get_air_quality(city)
     print(f"AQI in {city} is {air_quality_index}")
 
 if __name__ == "__main__":
