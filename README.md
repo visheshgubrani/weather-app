777d0595ac034703ddea8abad4bebfda899de640
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-05-20 14:23:46 +0530

    Added forecasr and AQ

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # weather-app
+
+Day 1 - Created the Inintial File 
+Display additional weather information: Expand your app to display more than just the current temperature. Retrieve and present data such as humidity, wind speed, weather condition, and more. You can access these values from the weather_data object returned by the API.
--- weather_app.py
+++ weather_app.py
@@ -2,18 +2,44 @@ import requests
 import json
 
 base_url = "http://api.weatherapi.com/v1"
-endpoint = "/current.json"
 api_key = "e69e523d9ed04abf8a2162555231705"
-country = input("Select the Country")
 
-request_url = f"{base_url}{endpoint}?key={api_key}&q={country}"
+def get_current_weather(country):
+    endpoint = "/current.json"
+    request_url = f"{base_url}{endpoint}?key={api_key}&q={country}"
+    response = requests.get(request_url)
+    weather_data = response.json()
+    current_temp = weather_data["current"]["temp_c"]
+    return current_temp
 
-response = requests.get(request_url)
+def get_forecast(city):
+    endpoint = "/forecast.json"
+    request_url = f"{base_url}{endpoint}?key={api_key}&q={city}"
+    response = requests.get(request_url)
+    forecast_data = response.json()
+    forecast_day = forecast_data["forecast"]["forecastday"][0]["date"]
+    forecast_temp = forecast_data["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
+    return forecast_day, forecast_temp
 
-weather_data = response.json()
+def get_air_quality(city):
+    endpoint = "/current.json"
+    request_url = f"{base_url}{endpoint}?key={api_key}&q={city}&aqi=yes"
+    response = requests.get(request_url)
+    air_quality_data = response.json()
+    air_quality_index = air_quality_data["current"]["air_quality"]["pm2_5"]
+    return air_quality_index
 
-current_temp = weather_data["current"]["temp_c"]
+def main():
+    country = input("Select the country: ")
+    city = input("Select the city: ")
 
-print(f"Temprature of {country} is {current_temp}C")
+    current_temp = get_current_weather(country)
+    forecast_day, forecast_temp = get_forecast(city)
+    air_quality_index = get_air_quality(city)
 
-city = input()
+    print(f"Temprature in {city}, {country} is {current_temp}C")
+    print(f"Forecast for {city} on {forecast_day} is {forecast_temp}C")
+    print(f"AQI in {city} is {air_quality_index}")
+
+if __name__ == "__main__":
+    main()
