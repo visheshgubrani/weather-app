ede829ba0880fb5f8a08f924a4e99eb365b0c66e
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-05-20 11:48:39 +0530

    AAdded the option for selecting countries

--- weather_app.py
+++ weather_app.py
@@ -4,15 +4,16 @@ import json
 base_url = "http://api.weatherapi.com/v1"
 endpoint = "/current.json"
 api_key = "e69e523d9ed04abf8a2162555231705"
+country = input("Select the Country")
 
-request_url = f"{base_url}{endpoint}?key={api_key}&q=India"
+request_url = f"{base_url}{endpoint}?key={api_key}&q={country}"
 
 response = requests.get(request_url)
 
 weather_data = response.json()
 
-current_temp = weather_data["current"]["temp_c"]["humidity"]
+current_temp = weather_data["current"]["temp_c"]
 
-print(f"Temprature of India is {current_temp}C")
+print(f"Temprature of {country} is {current_temp}C")
 
 city = input()
